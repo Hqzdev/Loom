8a595732b7dada30ff1eb17a54bd68fffd59945e
hqzdev <wkeyqwert@gmail.com>
2026-06-10 19:30:46 +0500

    Add Yandex Metrika counter

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 
 const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL ?? "https://useTether.dev";
 const GTM_ID = "GTM-WCSRPQFF";
+const YANDEX_METRIKA_ID = 109761424;
 
 export const metadata: Metadata = {
   metadataBase: new URL(SITE_URL),
@@ -240,6 +241,21 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           }}
         />
         <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+(function(m,e,t,r,i,k,a){
+  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+  m[i].l=1*new Date();
+  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+})(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=${YANDEX_METRIKA_ID}', 'ym');
+
+ym(${YANDEX_METRIKA_ID}, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+`,
+          }}
+        />
+        <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
@@ -253,6 +269,15 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             title="Google Tag Manager"
             width="0"
           />
+        </noscript>
+        <noscript>
+          <div>
+            <img
+              alt=""
+              src={`https://mc.yandex.ru/watch/${YANDEX_METRIKA_ID}`}
+              style={{ position: "absolute", left: "-9999px" }}
+            />
+          </div>
         </noscript>
         {children}
       </body>
